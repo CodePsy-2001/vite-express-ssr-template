3313bf21588318d3ae5c65eef285bf0e23669d16
CodePsy-2001 <81800589+CodePsy-2001@users.noreply.github.com>
2022-10-04 17:21:03 +0900

    Update index.page.server.ts

--- pages/index/index.page.server.ts
+++ pages/index/index.page.server.ts
@@ -1,21 +1,15 @@
 import { PageContext } from "!/renderer";
-import { mock } from '!/shop';
 
-export async function onBeforeRender({ session, urlParsed }: PageContext) {
+export async function onBeforeRender({ urlParsed }: PageContext) {
   const search = urlParsed?.search ?? {};
-  const category = search.category ?? '전체';
-
-  const { items } = mock;
-  items.sort((a, b) => a.price - b.price);
 
   return {
     pageContext: {
       pageProps: {
-        session,
-        items: items.filter(item => category === '전체' || item.category === category),
+        // 
       }
     }
   }
 }
 
-export const passToClient = ['pageProps', 'session'];
+export const passToClient = ['pageProps'];
